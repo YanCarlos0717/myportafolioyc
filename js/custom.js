6703c0f9e94e39e83579d2c07b06343a0c9fd99a
YanCarlos0717 <yhuertav@unasam.edu.pe>
2022-11-15 13:24:34 -0500

    marcaagua

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function( $ ) {
-
-  // Preloader
+    // Preloader
   $(window).on('load', function() {
     $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
   });
